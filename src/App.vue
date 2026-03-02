3ffbec3d529a7a634f9b2e7702d09a0c45b29b94
Benjamin Kabondo <benjaminkabondo@Benjamins-MacBook-Pro.local>
2026-03-02 03:11:02 -0600

    Conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,7 @@
 
   <div v-show="showAnswer">A keyboard.</div>
 
-  <!-- <div v-if="Math.random() > 0.5">Now you see me</div>
-  <div v-else>Now you don't</div> -->
+
 </template>
 
 <script setup>
